9786b4200ec3f4d7e27c6ee96b8c5c522e80fcb0
AdaLollA <lorenz.graaf@gmail.com>
2022-03-12 18:13:57 +0100

    tool?

--- scripts/pawn.gd
+++ scripts/pawn.gd
@@ -1,4 +1,21 @@
+@tool
 extends CharacterBody3D
+
+const COLOR_FRIENDY = Color('bfeaff')
+const COLOR_ENEMY = Color('ff9999')
+
+# 0  is player faction
+# >0 is enemies
+# <0 is allies
+@export var faction: int:
+	set(v):
+		# update color to represent faction
+		faction = v
+		if $Body:
+			if v == 0:
+				$Body.mesh.surface_get_material(0).albedo_color = COLOR_FRIENDY
+			elif v > 0:
+				$Body.mesh.surface_get_material(0).albedo_color = COLOR_ENEMY
 
 const SPEED = 5.0
 const JUMP_FORCE = 4.5
@@ -9,31 +26,33 @@ var selected = false
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		motion_velocity.y -= gravity * delta
+	if not Engine.is_editor_hint() and faction == 0:
+		# Add the gravity.
+		if not is_on_floor():
+			motion_velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		motion_velocity.y = JUMP_FORCE
+		# Handle Jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			motion_velocity.y = JUMP_FORCE
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		motion_velocity.x = direction.x * SPEED
-		motion_velocity.z = direction.z * SPEED
-	else:
-		motion_velocity.x = move_toward(motion_velocity.x, 0, SPEED)
-		motion_velocity.z = move_toward(motion_velocity.z, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			motion_velocity.x = direction.x * SPEED
+			motion_velocity.z = direction.z * SPEED
+		else:
+			motion_velocity.x = move_toward(motion_velocity.x, 0, SPEED)
+			motion_velocity.z = move_toward(motion_velocity.z, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
 
 func _on_pawn_input_event(camera, event, position, normal, shape_idx):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			handle_selection()
+	if not Engine.is_editor_hint() and faction == 0:
+		if event is InputEventMouseButton:
+			if event.pressed:
+				handle_selection()
 
 func handle_selection():
 	selected = !selected
