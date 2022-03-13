f489984f528da10dcc860fe8bd3870b80939b11a
AdaLollA <lorenz.graaf@gmail.com>
2022-03-13 13:30:48 +0100

    better projectile collision handling

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -6,7 +6,6 @@ func _on_max_lifetime_timeout():
 	queue_free()
 
 func _on_projectile_body_entered(body):
-	print('collision')
-	print(body)
-	queue_free()
-	# todo: only remove when hitting environment, otherwise handle removal
+	# remove when hitting environment, otherwise handle removal
+	if 'Map' == body.get_parent().name:
+		queue_free()
