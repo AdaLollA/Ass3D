8cdf6d6e5150fd02e96f238082f53d82aca9a7bc
AdaLollA <lorenz.graaf@gmail.com>
2022-03-21 17:32:30 +0100

    cleaner ui handling

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,11 +4,20 @@ func hide_all_windows():
 	for w in $Windows.get_children():
 		w.visible = false
 
+func untoggle_all_buttons_except(button: String):
+	for b in $Buttons.get_children():
+		if button not in b.name:
+			b.button_pressed = false
+
 func _on_work_toggled(button_pressed):
+	if button_pressed:
+		untoggle_all_buttons_except('Work')
 	hide_all_windows()
 	$Windows/Work.visible = button_pressed
 
 func _on_architect_toggled(button_pressed):
+	if button_pressed:
+		untoggle_all_buttons_except('Architect')
 	hide_all_windows()
 	$Windows/Architect.visible = button_pressed
 
